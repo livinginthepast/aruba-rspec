ddeb169c889b896c446a12cd229a8ba9728663fe
Eric Saxby <sax@livinginthepast.org>
2014-01-02 01:59:19 -0800

    Drop in stuffs from other projects

--- lib/aruba/rspec.rb
+++ lib/aruba/rspec.rb
@@ -1,7 +1,21 @@
-require "aruba/rspec/version"
+require 'aruba/rspec/version'
+
+require 'aruba-doubles'
+require 'aruba/rspec/matchers'
 
 module Aruba
   module RSpec
+    def self.history
+      @history ||= ArubaDoubles::History.new(File.join(ArubaDoubles::Double.bindir, ArubaDoubles::HISTORY_FILE))
+    end
 
+    def self.setup
+      ArubaDoubles::Double.setup
+    end
+
+    def self.teardown
+      ArubaDoubles::Double.teardown
+      history.clear
+    end
   end
 end
