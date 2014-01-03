b18424fb52c12a651cd546f61947847963848e7a
Eric Saxby <sax@livinginthepast.org>
2014-01-03 10:21:44 -0800

    Add stubbed specs around Aruba::RSpec module

--- spec/aruba/rspec_spec.rb
+++ spec/aruba/rspec_spec.rb
@@ -1,4 +1,32 @@
 require 'spec_helper'
 
 describe Aruba::RSpec do
+  describe '::history' do
+    it 'should be an ArubaDoubles::History' do
+      expect(Aruba::RSpec.history).to be_an(ArubaDoubles::History)
+    end
+  end
+
+  describe '::setup' do
+    it 'sets up ArubaDoubles' do
+      ArubaDoubles::Double.stub(:setup)
+      Aruba::RSpec.setup
+      expect(ArubaDoubles::Double).to have_received(:setup)
+    end
+  end
+
+  describe '::teardown' do
+    before { ArubaDoubles::Double.stub(:teardown) }
+
+    it 'tears down ArubaDoubles' do
+      Aruba::RSpec.teardown
+      expect(ArubaDoubles::Double).to have_received(:teardown)
+    end
+
+    it 'clears the history' do
+      Aruba::RSpec.history.stub(:clear)
+      Aruba::RSpec.teardown
+      expect(Aruba::RSpec.history).to have_received(:clear)
+    end
+  end
 end
